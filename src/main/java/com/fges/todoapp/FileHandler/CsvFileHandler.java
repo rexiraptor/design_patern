87f81f520f7c76ef92687efd4d7f076c1c02d2f0
rexiraptor <nicolas.dodartjr@gmail.com>
2024-01-23 21:08:40 +0100

    change code for warnings

--- src/main/java/com/fges/todoapp/FileHandler/CsvFileHandler.java
+++ src/main/java/com/fges/todoapp/FileHandler/CsvFileHandler.java
@@ -1,7 +1,5 @@
 package com.fges.todoapp.FileHandler;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
